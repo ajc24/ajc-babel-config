9e9a9e2f0c4a31cd6c0f149747a1b77f27cee2c2
ajc24 <anthonyjohnc@gmail.com>
2020-05-22 17:15:13 +0100

    add unit tests for the project

---
+++ tests/unit/babel.config.js
@@ -0,0 +1,75 @@
+import configureBabel from '../../src/babel-config';
+
+describe('Babel Configuration Module', () => {
+  const expectedBabelPlugins = [
+    '@babel/plugin-transform-arrow-functions'
+  ];
+  const expectedBabelPresets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '10'
+        }
+      }
+    ],
+    '@babel/preset-react'
+  ];
+
+  describe('configureBabel() method behaviour', () => {
+    let babelConfig;
+    let mockApi;
+
+    beforeAll(() => {
+      /* Create the data required for the test */
+      mockApi = {
+        cache: jest.fn()
+      };
+      babelConfig = configureBabel(mockApi);
+    });
+
+    afterAll(() => {
+      mockApi.cache.mockRestore();
+    });
+
+    it('verifies that the api.cache functionality is invoked', () => {
+      expect(mockApi.cache.mock.calls).toHaveLength(1);
+    });
+
+    it('verifies that a true value is provided as a parameter to the api.cache functionality', () => {
+      expect(mockApi.cache.mock.calls[0][0]).toBeTruthy();
+    });
+
+    it('verifies that the babel presets are set as part of the babel configuration', () => {
+      expect(babelConfig.presets).toBeDefined();
+    });
+    
+    it('verifies that the correct number of presets are set in the babel configuration', () => {
+      expect(babelConfig.presets.length).toBe(expectedBabelPresets.length);
+    });
+
+    it('verifies that the env preset is set to the list of presets in the babel configuration', () => {
+      expect(babelConfig.presets[0][0]).toBe(expectedBabelPresets[0][0]);
+    });
+
+    it('verifies that the target node is set to the env preset', () => {
+      expect(babelConfig.presets[0][1]).toStrictEqual(expectedBabelPresets[0][1]);
+    });
+
+    it('verifies that the react preset is set to the list of presets in the babel configuration', () => {
+      expect(babelConfig.presets[1]).toBe(expectedBabelPresets[1]);
+    });
+
+    it('verifies that the babel plugins are set as part of the babel configuration', () => {
+      expect(babelConfig.plugins).toBeDefined();
+    });
+
+    it('verifies that the correct number of plugins are set in the babel configuration', () => {
+      expect(babelConfig.plugins.length).toBe(expectedBabelPlugins.length);
+    });
+
+    it('verifies that the arrow functions transform plugin is set to the list of plugins in the babel configuration', () => {
+      expect(babelConfig.plugins[0]).toBe(expectedBabelPlugins[0]);
+    });
+  });
+});
