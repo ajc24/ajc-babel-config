e490c9559c00ba24a9a8180cca6bddaff2fc259b
ajc24 <anthonyjohnc@gmail.com>
2020-05-22 18:09:16 +0100

    update build 22/05/20

--- build/babel-config.js
+++ build/babel-config.js
@@ -18,7 +18,7 @@ const configureBabel = function (api) {
       node: '10'
     }
   }], '@babel/preset-react'];
-  const plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-arrow-functions'];
+  const plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-arrow-functions'];
   return {
     presets,
     plugins
